f55d1dbcc426fe7e95236895706c21c63b620656
yonash24 <yonashay87@gmail.com>
2025-03-20 16:00:31 +0200

    finish categorizer file

--- categorizer.py
+++ categorizer.py
@@ -1,7 +1,4 @@
-import os
-import json
-import hashlib
-
+"""Categorizes documents based on keywords"""
 
 class DocumentCategorizer:
     categories = {
@@ -12,12 +9,29 @@ class DocumentCategorizer:
     
     def categorize_document(self, text: str):
         """Determines the category of a document based on keywords"""
-        pass
+        if not text:
+            return "uncategorized"
+        
+        words = set(text.lower().split())
+        for category, keyword in self.categories.items():
+            if words & keyword:
+                return category
+        return "uncategoraized"
+        
+        
     
     def add_category(self, category_name: str, keyword_list: set):
         """Adds a new document category"""
-        pass
+        if category_name in self.categories:
+            return f"category: {category_name} already exist"
+        
+        self.categories[category_name] = set(keyword_list)
+        return f"category: {category_name} added successfully"
     
     def remove_category(self, category_name: str):
         """Removes a document category"""
-        pass
+        if category_name not in self.categories:
+            return f"category {category_name} does not exist"
+        
+        del self.categories[category_name]
+        return f"category {category_name} removed successfully"
